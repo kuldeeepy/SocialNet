39f6971113618ed4ad1b2a2777cbdeaba8ae9aec
Kuldeep yadav <kuldeeep.yadav1@gmail.com>
2024-04-17 19:15:22 +0530

    Update userRoute.js

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -42,8 +42,7 @@ router.post(
   "/login",
   wrapper(async (req, res) => {
     let { uname, pwd } = req.body;
-    if (uname || pwd)
-      return res.status(401).json({ message: "Provide all the details!" });
+
 
     let user = await User.findOne({ uname });
 
